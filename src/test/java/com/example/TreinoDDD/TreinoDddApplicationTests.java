2dc0f0ce9f53d1f31326345c9373797882c30fa4
BrenoXDMoon <brenogabriel19@gmail.com>
2022-01-17 13:24:51 -0300

    Add Projeto Spring

---
+++ src/test/java/com/example/TreinoDDD/TreinoDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TreinoDDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
