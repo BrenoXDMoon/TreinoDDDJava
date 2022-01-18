97fc29c46e0be02eda89ca79b80864e89c655b37
BrenoXDMoon <brenogabriel19@gmail.com>
2022-01-18 09:28:50 -0300

    Auditando para as operações de salvar, editar e excluir Users

--- src/main/java/com/example/TreinoDDD/service/impl/UserService.java
+++ src/main/java/com/example/TreinoDDD/service/impl/UserService.java
@@ -1,9 +1,11 @@
 package com.example.TreinoDDD.service.impl;
 
 import com.example.TreinoDDD.dto.UserDTO;
+import com.example.TreinoDDD.entity.Audithory;
 import com.example.TreinoDDD.entity.User;
 import com.example.TreinoDDD.facade.IFacade;
 import com.example.TreinoDDD.repositories.UserRepository;
+import com.example.TreinoDDD.service.IAudithoryService;
 import com.example.TreinoDDD.service.IUserService;
 import com.example.TreinoDDD.strategy.ValidateLastName;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,25 +21,34 @@ public class UserService implements IUserService {
     private UserRepository repository;
 
     @Autowired
+    private IAudithoryService audithoryService;
+
+    @Autowired
     private IFacade facade;
 
     @Override
     public User save(UserDTO dto) {
         ValidateLastName vln = new ValidateLastName();
         vln.process(dto);
-        return repository.saveAndFlush( (User) facade.convertToEntity(dto));
+        User user =  (User) facade.convertToEntity(dto);
+        audithoryService.save(new Audithory("Registro", user));
+        return repository.saveAndFlush(user);
     }
 
     @Override
     public User edit(UserDTO dto) {
         ValidateLastName vln = new ValidateLastName();
         vln.process(dto);
-        return repository.saveAndFlush( (User) facade.convertToEntity(dto));
+        User user =  (User) facade.convertToEntity(dto);
+        audithoryService.save(new Audithory("Alteração de Registro", user));
+        return repository.saveAndFlush(user);
     }
 
     @Override
     public void remove(UserDTO dto) {
-         repository.delete( (User) facade.convertToEntity(dto));
+        User user =  (User) facade.convertToEntity(dto);
+        audithoryService.save(new Audithory("Exclusão de Registro", user));
+        repository.delete(user);
     }
 
     @Override
